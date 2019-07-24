4b832a92b33ba116e78e5b8e43f4d50ed3585670
Michelle Lam <shelly2.lam@gmail.com>
2019-07-24 17:54:12 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -9,8 +9,10 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'byebug'
 
 group :development, :test do
+  gem 'byebug'
   gem 'rspec-rails'
   gem 'pry'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
+
+# class TitleValidator < ActiveModel::EachValidator
+#     def validate_each(record, title, value)
+#         unless value.match?(/\b(won't believe|secret|top.*[\d]|guess)\b/i)
+#             record.errors[attribute] << (options[:message] || "is not clickbait")
+#         end
+#     end
+# end
+
+
 class Post < ActiveRecord::Base
+    include ActiveModel::Validations
+    validates :title, presence: true
+    validate :post_has_to_be_click_bait
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Non-fiction Fiction)}
+
+    def post_has_to_be_click_bait
+        if !self.title.nil? && !self.title.match?(/\b(won't believe|secret|top.*[\d]|guess)\b/i)
+             errors.add(:title, "has to be in clickbait")
+         end
+    end
 end
