70eec5ef9cb00caffda00ad0014ac41f64c80e1a
Luke O'Malley <Lukeomalley95@gmail.com>
2019-07-24 15:48:20 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 50 }
+  validates :summary, length: { maximum: 50 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction], message: '%{value} is not a valid category' }
+  validate :clickbait?
+
+  def clickbait?
+    click_bait = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top [0-9]*/i,
+      /Guess/i
+    ]
+
+    if click_bait.none? { |pattern| pattern.match title }
+      errors.add :title, 'must be clickbait'
+    end
+  end
 end
