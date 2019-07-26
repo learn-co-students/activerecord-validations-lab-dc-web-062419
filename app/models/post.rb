52d671d10ba22c700f549ae34cf35caf261c412f
SkaMateria <skamateria@gmail.com>
2019-07-26 03:08:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :click_baity?
+
+    CLICKS = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+
+    def click_baity?
+        if CLICKS.none?  {|word| word.match title}
+            errors.add(:title, "CLICK HOLE!")
+        end
+    end
 end
